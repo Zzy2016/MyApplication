4481c386e723660c94d98dcbfe88acabcf6cfc18
zhangzeyang <zhangjie@huidr.com>
2020-03-05 17:07:56 +0800

    完善下载

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.FileProvider;
 
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -16,7 +17,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.PermissionRequest;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -31,11 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
     float totalSize;
     float currentSize;
-    String path = "";
+    String path = "https://huidr.oss-cn-hangzhou.aliyuncs.com/apk/DoctorApp-release.apk";
     File apkFile;
     String TAG = "CheckForUpdate";
     Uri uri;
-
+    TextView textView;
+    Dialog dialog;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +54,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        doPermission();
-        doDownloadApk();
+        textView = (TextView) findViewById(R.id.textview);
 
+        initDialog();
+        doPermission();
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        doDownloadApk();
+                    }
+                }).start();
+            }
+        });
+
+    }
+
+    public void initDialog() {
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_layout, null);
+        progressBar = view.findViewById(R.id.progressbar);
+        dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(view);
+        dialog.setCanceledOnTouchOutside(false);
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.width = (int) (display.getWidth() * 0.8);
+        window.setAttributes(lp);
     }
 
 
@@ -66,9 +106,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void checkVersion() {
-
-    }
 
     public void doDownloadApk() {
         try {
@@ -91,19 +128,18 @@ public class MainActivity extends AppCompatActivity {
             if (connection.getResponseCode() == 200) {
                 InputStream inputStream = connection.getInputStream();
                 BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-                byte[] buffer = new byte[1024];
+                byte[] buffer = new byte[1024 * 8];
                 int length = 0;
                 totalSize = connection.getContentLength();
                 FileOutputStream fileOutputStream = new FileOutputStream(apkFile);
-                Message message = new Message();
-                message.what = 0;
-                while ((length = bufferedInputStream.read(buffer)) != 0) {
+
+                while ((length = bufferedInputStream.read(buffer)) != -1) {
                     fileOutputStream.write(buffer, 0, length);
                     currentSize += length;
-                    int percent = (int) Math.floor(currentSize / totalSize) * 100;
-                    message.arg1 = percent;
+                    Message message = new Message();
+                    message.what = 0;
+                    message.arg1 = (int) Math.floor((currentSize / totalSize) * 100);
                     handler.sendMessage(message);
-
                 }
 
             } else {
@@ -120,27 +156,26 @@ public class MainActivity extends AppCompatActivity {
             super.handleMessage(msg);
             switch (msg.what) {
                 case 0:
+                    progressBar.setProgress(msg.arg1);
                     if (msg.arg1 == 100) {
                         doInstallApk();
-                    } else {
-
+                        dialog.cancel();
                     }
                     break;
             }
         }
     };
 
-    public void doInstallApk(){
-        Intent intent=new Intent(Intent.ACTION_VIEW);
-        if(Build.VERSION.SDK_INT> Build.VERSION_CODES.N){
-            uri= FileProvider.getUriForFile(MainActivity.this,getPackageName()+".myFilePRovider",apkFile);
-            intent.setDataAndType(uri,"application/vnd.android.package-archive");
+    public void doInstallApk() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            uri = FileProvider.getUriForFile(MainActivity.this, getPackageName() + ".myFileProvider", apkFile);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        }else{
-            intent.setDataAndType(uri,"application/vnd.android.package-archive");
+        } else {
+            uri = Uri.fromFile(apkFile);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
+        intent.setDataAndType(uri, "application/vnd.android.package-archive");
         startActivity(intent);
     }
-
 }
